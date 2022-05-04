7a42019d7cc7bc2f7d17dafac629ae20262f3693
Bahauddin-Fahad <bahauddin.fahad819@gmail.com>
2022-05-04 22:16:57 +0600

    data added

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// User: SoundHub-server
+// PASS: SKf38JMGGE5MQGyT;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clustersoundhub.abk9x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const inventoryCollection = client.db("soundHub").collection("inventories");
+
+    // Getting All The inventories from DB
+    app.get("/inventories", async (req, res) => {
+      const query = {};
+      const cursor = inventoryCollection.find(query);
+      const inventories = await cursor.toArray();
+      res.send(inventories);
+    });
+
+    // Getting a single inventory item from db
+    app.get("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const inventory = await inventoryCollection.findOne(query);
+      res.send(inventory);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running Sound Hub Server");
+});
+
+app.listen(port, () => {
+  console.log("Listening To The Sound Hub Server");
+});
